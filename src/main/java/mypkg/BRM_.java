23e0a1be5fc12af904950981a1fc0d1473237a4c
vasukansal <vasu12b8075@gmail.com>
2023-02-06 23:45:17 +0530

    First commit jenkins-Maven(Lab4)

---
+++ src/main/java/mypkg/BRM_.java
@@ -0,0 +1,10 @@
+package mypkg;
+public class BRM_ {
+	public void display() {
+		System.out.print("HELLO");
+	}
+	public static void main(String[] args) {
+		BRM_ obj=new BRM_();
+		obj.display();
+	}
+}
